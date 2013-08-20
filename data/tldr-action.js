174cb68e847bc4e7b94c6293be2c27ab7ab017c3
vayan <yann@vaillant.im>
2013-08-21 00:34:21 +0200

    loading icon, fix, im bad

--- data/tldr-action.js
+++ data/tldr-action.js
@@ -1,8 +1,16 @@
 var tldrcontent = document.getElementById("tldr-content");
 
 self.on("message", function(addonMessage) {
+	if (addonMessage === "fail") {
+		tldrcontent.innerHTML = 'Failed...sorry';
+		return;
+	}
   tldrcontent.innerHTML = '';
   for (var key = 0; key < addonMessage.length; ++key) {
     tldrcontent.innerHTML += "<li>"+addonMessage[key]+"</li>";
     }
+});
+
+self.port.on("show", function(data) {
+  tldrcontent.innerHTML = '<img src="'+data+'" />';
 });
